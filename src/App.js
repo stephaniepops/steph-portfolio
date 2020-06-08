39dfbbc3814cab259b458c838a14f6ac2223d413
stephaniepops <59659213+stephaniepops@users.noreply.github.com>
2020-06-08 11:20:26 -0500

    Add description div

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import "./App.css";
 
 import Navbar from "./Components/Navbar";
 import PhotoName from "./Components/PhotoName";
+import Description from "./Components/Description";
 
 function App() {
   return (
@@ -11,18 +12,7 @@ function App() {
       <header className="App-header">
         <Navbar />
         <PhotoName />
-
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Description />
       </header>
     </div>
   );
---
+++ src/Components/Description.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Description = () => {
+  return (
+    <div>
+      (Placeholder text) I am currently studying Applied Computer Science at the
+      University of Winnipeg. This portfolio was created as a way to expand my
+      understanding of React, and practice the different skills I've learning
+      during independent studies. I am looking to secure employment as a coop
+      student for the fall semester.
+    </div>
+  );
+};
+
+export default Description;
