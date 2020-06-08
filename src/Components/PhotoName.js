c960d7a6f5b52873f42c5df859706bc4f523ea2e
stephaniepops <59659213+stephaniepops@users.noreply.github.com>
2020-06-08 11:06:45 -0500

    Adding Name and Photo div
    placeholder information - rough outline portfolio photo and name

---
+++ src/Components/PhotoName.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ProfilePhoto from "../images/profile.jpg";
+
+const PhotoName = () => {
+  return (
+    <div>
+      <img src={ProfilePhoto} className="" alt="" />
+      <h2>steph.pop(s)</h2>
+    </div>
+  );
+};
+
+export default PhotoName;
