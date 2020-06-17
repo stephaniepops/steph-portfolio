69db4067c73bb1ad65b974c26ac3dc362b16a18a
stephaniepops <59659213+stephaniepops@users.noreply.github.com>
2020-06-17 11:02:29 -0500

    bug fix

--- src/App.js
+++ src/App.js
@@ -8,14 +8,6 @@ import Contact from "./components/Contact";
 import ProjectContainer from "./components/ProjectContainer";
 import { Projects } from "./data/UserInfo";
 
-import Navbar from "./components/Navbar";
-import PhotoName from "./components/PhotoName";
-import Description from "./components/Description";
-import Skills from "./components/Skills";
-import Contact from "./components/Contact";
-import ProjectContainer from "./components/ProjectContainer";
-import Projects from "./components/Projects";
-
 const Wrapper = styled.section`
   background: #f1f1f3;
   padding: 15px;
