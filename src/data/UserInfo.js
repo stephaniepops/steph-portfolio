f6f5d3a719cf0792771217d4abfb254c4d15b044
stephaniepops <59659213+stephaniepops@users.noreply.github.com>
2021-02-27 12:04:46 -0600

    Add new skills and project

--- src/data/UserInfo.js
+++ src/data/UserInfo.js
@@ -1,10 +1,15 @@
 import PortfolioProject from "../images/LogoStephFlip.jpg";
+import FavouriteMovieProject from "../images/Favourite Movie Finder Screenshot.PNG";
 import figma from "../images/logos/figma.svg";
 import framer from "../images/logos/framer.svg";
 import git from "../images/logos/git.svg";
 import java from "../images/logos/java.svg";
 import node from "../images/logos/node-dot-js.svg";
 import react from "../images/logos/react.svg";
+import redux from "../images/logos/redux.svg";
+import postgresql from "../images/logos/postgresql.svg";
+import aws from "../images/logos/amazonaws.svg";
+import serverless from "../images/logos/serverless.svg";
 import styledComponents from "../images/logos/styled-components.svg";
 import javaScript from "../images/logos/javascript.svg";
 
@@ -15,19 +20,26 @@ export const Projects = [
       "After completing a number of React tutorials on Team Treehouse, I decided to practice the skills I learned by creating my portfolio.",
     techStack: ["React", "JavaScript", "Styled Components", "Framer Motion"],
     image: PortfolioProject,
-    imageDescription: "",
+    imageDescription: "Logo image from portfolio",
     codeLink: "https://github.com/stephaniepops/steph-portfolio",
     demoLink: null,
   },
   {
-    title: "Portfolio Project",
+    title: "Favourite Movie Finder",
     description:
-      "After completing a number of React tutorials on Team Treehouse, I decided to practice the skills I learned by creating my portfolio.",
-    techStack: ["React", "JavaScript", "Styled Components", "Framer Motion"],
-    image: PortfolioProject,
-    imageDescription: "",
-    codeLink: "https://github.com/stephaniepops/steph-portfolio",
-    demoLink: null,
+      "A small project I created using the OMDB open API to explore Redux, Chakra UI and Formik",
+    techStack: [
+      "React",
+      "JavaScript",
+      "Styled Components",
+      "Redux",
+      "Chakra  UI",
+      "Formik",
+    ],
+    image: FavouriteMovieProject,
+    imageDescription: "Screenshot of Favourite Movie Finder app",
+    codeLink: "https://github.com/stephaniepops/OMDB-API-Test",
+    demoLink: "https://favouritemoviefinder.netlify.app/",
   },
 ];
 
@@ -70,6 +82,26 @@ export const SkillList = [
   {
     name: "Framer Motion",
     image: framer,
+    url: "",
+  },
+  {
+    name: "Redux",
+    image: redux,
+    url: "",
+  },
+  {
+    name: "PostgreSQL",
+    image: postgresql,
+    url: "",
+  },
+  {
+    name: "Amazon Web Services",
+    image: aws,
+    url: "",
+  },
+  {
+    name: "Serverless",
+    image: serverless,
     url: "",
   },
 ];
